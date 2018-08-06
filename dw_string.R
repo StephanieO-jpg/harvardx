1fa2d03ca15267b3273010078fdb7becaeb7f723
Trevor <Trevor@iMac.local>
2018-08-06 19:55:55 +0200

    Latest file update

--- dw_string.R
+++ dw_string.R
@@ -26,3 +26,23 @@ yes <- c("5'7\"","6'2\"")
 no <- c("6,2\"","6.2\"")
 str_detect(yes, pattern)
 str_detect(no,pattern)
+
+animals <- c("cat", "puppy", "Moose", "MONKEY")
+pattern_animals <- "[A-Z]$"
+str_detect(animals, pattern_animals)
+
+animals2 <- c("moose", "monkey", "meerkat", "mountain lion")
+pattern2 <- "mo?"
+str_detect(animals2, pattern2)
+
+schools <- c("U. Kentucky","Univ New Hampshire","Univ. of Massachusetts","University Georgia","U California")
+final <- schools %>% 
+  str_replace("^Univ\\.?\\s|^U\\.?\\s", "University ") %>% 
+  str_replace("^University of |^University ", "University of ")
+final
+
+problems <- c("5.3", "5,5", "6 1", "5 .11", "5, 12")
+pattern_with_groups <- "^([4-7])[,\\.\\s](\\d*)$"
+str_replace(problems, pattern_with_groups, "\\1'\\2")
+
+
